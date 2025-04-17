df35fa98613714b0abc4d030f5b77c363eee2223
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2025-04-16 20:59:08 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Hafiz Washington',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: "Jasmine Concord",
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'John patman',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Netsarul Jepta',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Mathew white',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Arjun Singh',
+    avatar: 'user-6.jpg',
+  },
+];
