15ac7f47ebd60f4aa940ed2d7474011606636eca
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2025-04-17 11:25:36 -0700

    DefiningInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent, UserComponent],
@@ -8,5 +10,5 @@ import { UserComponent } from "./user/user.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-angular-app';
+ users = DUMMY_USERS;
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,13 +10,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input() avatar!: string;
+  @Input() name!: string;
+  @Input() id!: number;
+  // selectedUser = signal(DUMMY_USERS)
+  // selectedUser = signal(DUMMY_USERS[randomIndex]); 
+   //initalize here 
+  // imagePath = computed(()=> 'assets/users/' + this.selectedUser().avatar);
+  // what is computed?
+  // Computed is function that creates reactive value that automaticly updates when it's depencies changes
+  // it mean when we add a photo  or anything changes it recalculate, check signal is changed so it will uppdate the UI.
+   
   onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set( DUMMY_USERS[randomIndex])  //updating here using set()
+   
   }
-  showDetails = false;
-  onClickButton(){
-    this.showDetails = !this.showDetails;
-  }
+ 
 }
